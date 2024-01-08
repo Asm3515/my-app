da9dc071d5f7abf244f4d48f7a34df606bb10b4d
Asm3515 <ajinkya.more3515@gmail.com>
2024-01-07 23:03:14 -0500

    Navbar is now responsive

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -1,51 +1,40 @@
 // Navbar.jsx
 
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.css';
-import { useState } from 'react';
-import "../Section/Section.css"
-
+import "../Section/Section.css";
 
 export const Navbar = () => {
-    const [menuOpen, setMenuOpen] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
 
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
   };
+
   const scrollToSection = (sectionId) => {
     const section = document.getElementById(sectionId);
     if (section) {
       section.scrollIntoView({ behavior: 'smooth' });
     }
   };
+
   return (
-    <nav>
     <nav>
       <div className="logo">
         <div className="name">Ajinkya More</div>
-        <ul className="nav-links">
-              <li><a onClick={() => scrollToSection('about')}>About</a></li>
-              <li><a onClick={() => scrollToSection('experience')}>Experience</a></li>
-              <li><a onClick={() => scrollToSection('projects')}>Projects</a></li>
-              <li><a onClick={() => scrollToSection('contact')}>Contact</a></li>
+        <ul className={`nav-links ${menuOpen ? 'open' : ''}`}>
+          <span
+            id="hamburger-menu-ideal"
+            onClick={toggleMenu}
+            className={`hamburger-span ${menuOpen ? 'open' : ''}`}
+          ></span>
+
+          <li className='hamburger-links'><a onClick={() => scrollToSection('about')}>About</a></li>
+          <li className='hamburger-links'><a onClick={() => scrollToSection('experience')}>Experience</a></li>
+          <li className='hamburger-links'><a onClick={() => scrollToSection('projects')}>Projects</a></li>
+          <li className='hamburger-links'><a onClick={() => scrollToSection('contact')}>Contact</a></li>
         </ul>
       </div>
-      {/* <nav className='hamburger-nav'>
-        <div className='hamburger-menu'>
-            <div className='hamburger-icon' onClick={toggleMenu}>
-                <span></span>
-                <span></span>
-                <span></span>
-                <div className={`menu-links ${menuOpen ? 'false' : ''}`}>
-                    <li><a href='/about' onClick={toggleMenu}>About</a></li>
-                    <li><a href='/experience' onClick={toggleMenu}>Experience</a></li>
-                    <li><a href='/projects' onClick={toggleMenu}>Projects</a></li>
-                    <li><a href='/contact' onClick={toggleMenu}>Contact</a></li>
-                </div>
-            </div>
-        </div>
-      </nav> */}
-    </nav>
     </nav>
   );
 };
